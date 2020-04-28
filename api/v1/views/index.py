0f7a8212c6008d25513c51c9e6af042a8e795c8e
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-28 23:53:46 +0000

    [Feature] Method is added to return number of objects by class. modified:   api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,18 @@
 """ """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route("/status")
 def return_status():
     "status return"
     return jsonify({'status': 'OK'})
+
+@app_views.route("/stats")
+def return_stats():
+    "return stats "
+    dict = {}
+    name_clases = ["Amenity", "City", "Place", "Review", "State", "User"]
+    for i in name_clases:
+        dict[i] = storage.count(i);
+    return jsonify(dict)
