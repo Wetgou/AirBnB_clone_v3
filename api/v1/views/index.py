52970984e536cf36d2b4e72a5e17e55c28494e6a
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-29 02:15:04 +0000

    [Fix] Format json is modified. modified:   views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-""" """
+'''
+routes:
+/status - returns status: Ok
+/stats - returns the number of instance
+'''
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'])
-def return_status():
-    "status return"
+@app_views.route('/status')
+def status():
+    """Create status return"""
     return jsonify({'status': 'OK'})
 
 
-@app_views.route("/stats")
-def return_stats():
-    "return stats "
-    dict = {}
-
-    name_clases = ["Amenity", "City", "Place", "Review", "State", "User"]
-    for i in name_clases:
-        dict[i] = storage.count(i)
-    return jsonify(dict)
+@app_views.route('/stats')
+def stats():
+    '''Returns the classes with their instance counts'''
+    stat = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stat)
