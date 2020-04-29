dee5785cdb49183c216872b900a31515382842cc
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-29 02:36:59 +0000

    [Fix] The line of comment is fixed. modified:   app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Python api built with flask"""
-from flask import Flask, jsonify
+""" Script that starts a flask application in a port 5000 """
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -12,17 +14,17 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def tear_down(exception):
-    '''Calls storage close on appcontext'''
+    """Calls Storage close on appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def error_handler(error):
-    '''Returns a JSON formatted 404 status code'''
+def handler_404(error):
+    """ Response to not found """
     return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
     app.run(host=host, port=int(port), threaded=True)
