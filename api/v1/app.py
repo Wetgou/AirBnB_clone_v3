ee19577e3d2cc107e523fa1ef5d384601aea2c80
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-29 02:30:54 +0000

    [Fix]  The order is changed. modified:   app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-# Script that starts a flask application in a port 5000
-from flask import Flask
+"""Python api built with flask"""
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import jsonify
-
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -14,17 +12,17 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def tear_down(exception):
-    """Calls Storage close on appcontext"""
+    '''Calls storage close on appcontext'''
     storage.close()
 
 
 @app.errorhandler(404)
-def handler_404(error):
-    """ Response to not found """
+def error_handler(error):
+    '''Returns a JSON formatted 404 status code'''
     return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=int(port), threaded=True)
