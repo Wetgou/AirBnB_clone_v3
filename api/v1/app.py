104c4240738865b32d943330745ab3158813209f
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-04-30 03:25:21 +0000

    [Feature] CORS is implemented to allow cross origin. modified:   api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
