5dc15ee0c02f47207ac0a4b220ec439e9b0599e3
Manuel Mosquera <mosqueramanuel5@gmail.com>
2020-05-02 22:05:59 +0000

    [Feature] md5 is added to save passwords of Object Users. modified:   models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import hashlib
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -31,6 +32,10 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
+                    if key == 'password':
+                        md5_encode = hashlib.md5()
+                        md5_encode.update(bytes(value, "utf-8"))
+                        value = md5_encode.digest()
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
@@ -68,6 +73,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if models.storage_t == "db" and "password" in new_dict:
+            del new_dict['password']
         return new_dict
 
     def delete(self):
